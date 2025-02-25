df1f6d8255ac35297e006d293e56637f6d0cf41b
Aru Moon <anton17082003@gmail.com>
2025-02-26 04:19:31 +0500

    Update xash entrypoint.sh

--- games/xash/entrypoint.sh
+++ games/xash/entrypoint.sh
@@ -33,10 +33,10 @@ if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
     # Update Source Server
     if [ ! -z ${GLDSRC_APPID} ]; then
         ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} +app_update ${GLDSRC_APPID} $( [[ -z ${GLDSRC_BETAID} ]] || printf %s "-beta ${GLDSRC_BETAID}" ) $( [[ -z ${GLDSRC_BETAPASS} ]] || printf %s "-betapassword $GLDSRC_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
-        wget https://github.com/FWGS/xash3d-fwgs/releases/download/continuous/xashds-linux-i386.tar.gz
-		tar -zxvf xashds-linux-i386.tar.gz && rm xashds-linux-i386.tar.gz
-		mv xashds-linux-i386/* .
-		rmdir xashds-linux-i386/
+        wget https://github.com/FWGS/xash3d-fwgs/releases/download/continuous/xashds-linux-amd64.tar.gz
+		tar -zxvf xashds-linux-amd64.tar.gz && rm xashds-linux-amd64.tar.gz
+		mv xashds-linux-amd64/* .
+		rmdir xashds-linux-amd64/
     else
         echo -e "No appid set. Starting Server"
     fi
